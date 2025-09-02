aef2da810d1486849a587af2330d4e5b878c097e
PhakawatDev <phakawat10@gmail.com>
2025-09-02 14:49:40 +0700

    Layout

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,44 @@
 'use client'
 
 import { useState } from "react"
+import { config } from "../config";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
 
 export default function SignIn(){
     const [username,setUsername] = useState('');
     const [password,setPassword] = useState('');
+
+    const router = useRouter();
+    const handleSignIn =async () => {
+        try {
+            const payload = {
+                username: username,
+                password: password,
+            };
+            console.log(`${config.apiUrl}/user/signin`);
+            const response = await axios.post(`${config.apiUrl}/user/signin`,payload)
+
+            if (response.data.token !== null){
+                localStorage.setItem('token',response.data.token)
+                router.push('backoffice/dashboard');
+            }else {
+                Swal.fire({
+                    title: 'ตรวจสอบ user',
+                    text: 'ชื่อผู้ใช้งาน หรือ รหัสผ่านไม่ถูกต้อง',
+                    icon: 'warning',
+                    timer: 2000
+                })
+            }
+        } catch (error: any) {
+            Swal.fire({
+                title:'Error',
+                text: error.mesage,
+                icon: 'error'
+            })
+        }
+    };
 
     return (
     <div className="signin-container">
@@ -22,7 +56,7 @@ export default function SignIn(){
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}/>
 
-            <button className="mt-4">
+            <button className="mt-4" onClick={handleSignIn}>
                 Sign In
             </button>
         </div>
